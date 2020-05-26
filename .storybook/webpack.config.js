9f066b0ce9c2e5d0c463b0ba74c9645399e527ee
Lyn Ho <lyn4develop@gmail.com>
2020-05-27 01:18:18 +0800

    stroy source

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,5 +5,11 @@ module.exports = ({ config }) => {
     enforce: 'post',
   })
 
+  config.module.rules.push({
+    test: /\.stories\.js?$/,
+    loaders: [require.resolve('@storybook/addon-storysource/loader')],
+    enforce: 'pre',
+  })
+
   return config
 }
